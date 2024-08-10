b8ccedf6c1e351cc570ad52a0f4c72fbe8b25af7
Sanjay Menaria <76099779+sanjaymenaria03@users.noreply.github.com>
2024-08-10 15:15:46 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+set -e
 
+# Pull the Docker image from Docker Hub
+docker pull mightysanjay/simple-python-flask-app
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 mightysanjay/simple-python-flask-app
