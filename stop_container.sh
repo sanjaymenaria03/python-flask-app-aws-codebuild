713f638b1ca25d2e530427740e274866dbde1912
Sanjay Menaria <76099779+sanjaymenaria03@users.noreply.github.com>
2024-08-10 15:41:05 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 set -e
 
+
+
 # Stop the running container (if any)
-echo "Hi"
+
+# Get the container ID of the running Docker container
+container_id=$(docker ps -q)
+
+# Check if a container ID was found
+if [ -n "$container_id" ]; then
+    echo "Stopping container with ID: $container_id"
+    docker stop "$container_id"
+else
+    echo "No running Docker container found."
+fi
